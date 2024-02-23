26b2ebee60fde1fd557628daae43c9cc72869105
yuban <2471998500@qq.com>
2024-02-23 14:25:49 +0800

    初始化2

---
+++ 后端/yubanSever/src/main/java/com/yuban/yubansever/YubanSeverApplication.java
@@ -0,0 +1,13 @@
+package com.yuban.yubansever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YubanSeverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YubanSeverApplication.class, args);
+    }
+
+}
